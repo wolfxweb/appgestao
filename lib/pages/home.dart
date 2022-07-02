fa8825d92c00662062b57608cdf819af93ee6ec6
Eduardo lobo <wolfxweb@gmail.com>
2022-07-02 14:32:58 -0300

    Configurado fire base

---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: const Center(
+        child: Text('home'),
+      ),
+    );
+  }
+}
---
+++ lib/splash_page.dart
@@ -0,0 +1,61 @@
+
+
+import 'package:appgestao/pages/home.dart';
+import 'package:appgestao/usuaruio/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState()async{
+    super.initState();
+
+    //FirebaseFirestore.instance.collection("usuario").doc('email').set({"texto":"teste"});
+
+
+    Future futureStart =  Future.delayed(const Duration(seconds: 3));
+
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((User? user) {
+      if (user == true) {
+        print('User is currently signed out!');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Login()),
+        );
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Home()),
+        );
+      }
+    });
+    Future.wait([futureStart]).then((value) {
+
+    });
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amberAccent,
+      child: const Center(
+        child: CircularProgressIndicator(),
+      ),
+
+    );
+  }
+}
