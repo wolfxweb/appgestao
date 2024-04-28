d4eb575961178801dd2fcf2525a1ba2b6a125e89
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2024-04-28 10:33:16 -0300

    ajuste cores

--- lib/componete/headerAppBar.dart
+++ lib/componete/headerAppBar.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
+
 class HeaderAppBar {
   getAppBar(String title) {
     return AppBar(
       title: Text(
         title,
-        style: const TextStyle(fontSize: 20,
-          fontWeight:
-          FontWeight.bold,
-            fontFamily:'Arial' ,
-            color: Colors.white ),
+        style: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Arial',
+          color: Colors.white, // Cor do texto
+        ),
       ),
       centerTitle: true,
-      backgroundColor: const Color.fromRGBO(1, 57, 44,1),
-   //   actionsIconTheme: const IconThemeData(color: Colors.white),
-
-      //  elevation: 15,
-      //  toolbarHeight: 100, // default is 56
-      //  toolbarOpacity: 0.5,
+      backgroundColor: const Color.fromRGBO(1, 57, 44, 1),
+      iconTheme: const IconThemeData(color: Colors.white), // Cor do ícone
     );
   }
 }
